76b50241e1e121fa078bf7ae422e2c632ef25c9b
raggi <jftucker@gmail.com>
2009-12-29 13:11:19 +0000

    Fix your load path in the loader, not the lib

--- lib/qusion.rb
+++ lib/qusion.rb
@@ -1,16 +1,10 @@
 # encoding: UTF-8
-unless defined?(QUSION_ROOT)
-  QUSION_ROOT = File.dirname(__FILE__) + '/'
-end
-
 require "eventmachine"
 require "mq"
 
-require QUSION_ROOT + "qusion/server_spy"
-require QUSION_ROOT + "qusion/em"
-require QUSION_ROOT + "qusion/amqp"
-require QUSION_ROOT + "qusion/channel_pool"
-require QUSION_ROOT + "qusion/amqp_config"
+require "qusion/server_spy"
+require "qusion/channel_pool"
+require "qusion/amqp_config"
 
 module Qusion
   def self.start(*opts)
