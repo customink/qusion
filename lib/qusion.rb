c92d2b59741a8f9ac7515e41622c6a5f840d680e
raggi <jftucker@gmail.com>
2009-12-29 13:10:07 +0000

    Remove unnecessary monkey patches

--- lib/qusion.rb
+++ lib/qusion.rb
@@ -15,9 +15,42 @@ require QUSION_ROOT + "qusion/amqp_config"
 module Qusion
   def self.start(*opts)
     amqp_opts = AmqpConfig.new(*opts).config_opts
-    AMQP.start_web_dispatcher(amqp_opts)
+    start_amqp_dispatcher(amqp_opts)
   end
   
+  def self.start_amqp_dispatcher(amqp_settings={})
+    AMQP.settings.merge!(amqp_settings)
+    case Qusion::ServerSpy.server_type
+    when :passenger
+      PhusionPassenger.on_event(:starting_worker_process) do |forked| 
+        if forked
+          EM.stop if EM.reactor_running?
+          Thread.current[:mq] = nil
+          AMQP.instance_variable_set(:@conn, nil)
+        end
+        @thread = Thread.new { start }
+        die_gracefully_on_signal
+      end
+    when :standard
+      @thread = Thread.new { start }
+      die_gracefully_on_signal
+    when :evented
+      die_gracefully_on_signal
+    when :none
+    else
+      raise ArgumentError, "AMQP#start_web_dispatcher requires an argument of [:standard|:evented|:passenger|:none]"
+    end
+    if @thread
+      @thread.abort_on_exception = true
+      Thread.pass until EM.reactor_running?
+    end
+  end
+
+  def self.die_gracefully_on_signal
+    Signal.trap("INT")  { EM.schedule { AMQP.stop { EM.stop } } }
+    Signal.trap("TERM") { EM.schedule { AMQP.stop { EM.stop } } }
+  end
+
   def self.channel
     ChannelPool.instance.channel
   end
